7e5542c1f186505dd8a0ccd36ae04b3de14c91b1
tebibux <tebibusolomon79@gmail.com>
2022-06-02 15:43:58 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,11 @@
  *main - begining of excution
  *
  *Return: 1
- *
  */
 int main(void)
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	
-	write(2, msg, 59);
+	write( 2, msg, 59);
 	return (1);
 }
