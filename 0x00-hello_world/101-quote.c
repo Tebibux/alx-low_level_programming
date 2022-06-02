392d4cab85fe31170979603849386e9d3a141bc4
tebibux <tebibusolomon79@gmail.com>
2022-06-02 15:10:38 -0700

    Task 8, restricted printf, puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - begining of excution
+ *
+ *return: 1
+ *
+ */
+int main(void)
+{
+	char str1[] = "and that piece of art is useful\" - Dora Karpar, 2015-10-19\n;
+	write(str1);
+	return (1);
+}
