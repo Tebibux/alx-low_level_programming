6574e31de035d0ff027a04860c7e1778f4441699
tebibux <tebibusolomon79@gmail.com>
2022-06-02 15:14:59 -0700

    Task 8, restricted printf, puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora Karpar, 2015-10-19\n;
-	write(str1);
+	char msg[] = "and that piece of art is useful\" - Dora Karpar, 2015-10-19\n";
+	write(2, msg, sizeof(msg));
 	return (1);
 }
