f526e9dcbd9f875570262ef83fd3fbf4dd7752e1
tebibux <tebibusolomon79@gmail.com>
2022-06-02 15:23:36 -0700

    Task 8, restricted printf, puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  *main - begining of excution
  *
  *Return: 1
  *
  */
-
 int main(void)
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
