dd09674fb3f46f3d287ed45d8da909e85dd8db5d
tebibux <tebibusolomon79@gmail.com>
2022-06-03 15:36:13 -0700

    Task 5, Printing number less 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number single digit to the bese 10 starting from 0
+ *
+ * Return: 0 if succuss
+ *
+ */
+int main(void)
+{
+	int i = 0;
+	while (i < 10)
+	{
+		printf(i);
+	}
+	printf('\n');
+	return (0);
+}
