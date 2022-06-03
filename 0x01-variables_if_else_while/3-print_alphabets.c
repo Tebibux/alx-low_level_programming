c6b1458e9b1b8e532b9f71fcc1eaf39fe95b0335
tebibux <tebibusolomon79@gmail.com>
2022-06-03 15:18:38 -0700

    Task 3, upper and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lower case and upper case respectively
+ *
+ * Return - 0
+ */
+int main(void)
+{
+	char lower_case[26] = "abcdefghijklmnopqrstuvwxyz";
+	char upper_case[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+	int j;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(lower_case[i]);
+	}
+	for (j = 0; j < 26; j++)
+	{
+		putchar(upper_case[j]);
+	}
+
+	putchar('\n');
+	return (0);
+}
