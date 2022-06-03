11cc13fcf7e9e1ca105548262502fa55421f57d9
tebibux <tebibusolomon79@gmail.com>
2022-06-03 16:05:35 -0700

    Task 7, revers text

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Prints reversed alphabet
+ *
+ * Return: 0 if Success
+ */
+int main(void)
+{
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 25, i >= 0, i--)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
