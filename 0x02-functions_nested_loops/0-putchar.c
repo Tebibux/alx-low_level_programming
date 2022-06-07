0c7814d784faf77b2662d520b5872ca5f66c9129
tebibux <tebibusolomon79@gmail.com>
2022-06-07 15:54:33 -0700

    whigt space

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,9 @@
 int main(void)
 {
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int strt, sz;	
+	int strt;
+	int sz;
+
 	sz = sizeof(str) / sizeof(int);
 	for (strt = 0; strt < sz; strt++)
 	{
