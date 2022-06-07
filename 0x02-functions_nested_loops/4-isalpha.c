02e2559ed02459e5894b50dfc86631d7ebf17c81
tebibux <tebibusolomon79@gmail.com>
2022-06-07 13:16:57 -0700

    After cheking betty code style

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,17 +10,17 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 65 && c <= 90) 
 	{
 		return (1);
 	}
-	else if (c >= 97 && c <= 122)
+	else if (c >= 97 && c <= 122) 
         {
-                return (1);
+		return (1);
         }
-        else
+        else 
         {
-                return (0);
+		return (0);
         }
         _putchar('\n');
 }
