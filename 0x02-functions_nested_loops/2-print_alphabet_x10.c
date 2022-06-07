c05fcb0987564908b292fa8954c3cd1fa37d527e
tebibux <tebibusolomon79@gmail.com>
2022-06-07 12:41:21 -0700

    Task 2, alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - it prints alphabet a-z 10x
+ *
+ * Return: 0 if success
+ */
+void print_alphabet_10x(void)
+{
+	int i;
+	int j;
+
+	for (j = 1; j <= 10; j++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
