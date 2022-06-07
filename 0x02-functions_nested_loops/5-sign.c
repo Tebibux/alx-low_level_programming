f2c4f592853d6d287c2d66b08c135a4a9596e69a
tebibux <tebibusolomon79@gmail.com>
2022-06-07 13:40:55 -0700

    After assignment

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,7 +7,7 @@
  *
  * Return - 1 if @n > 0, 0 if @n == 0 and -1 if @n < 0
  */
- int print_sign(int n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
@@ -19,9 +19,11 @@
 		_putchar('0');
 		return (0);
 	}
+	int t;
+	t = 0-n;
 	else
 	{
-		_putchar('-1');
+		_putchar('%d', t);
 		return (-1);
 	}
 	_putchar('\n');
