666f77249ae10939fdf476e7b0220542e8c2c04a
tebibux <tebibusolomon79@gmail.com>
2022-06-07 12:26:33 -0700

    Task 1, printing alphabet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * main - Prints _putchar as a message 
+ * main - Prints _putchar as a message
  *
  * Return: On 0 if success
  */
 int main(void)
 {
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int strt, sz;
-	
+	int strt, sz;	
 	sz = sizeof(str) / sizeof(int);
 	for (strt = 0; strt < sz; strt++)
 	{
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - extution start and print alphabet
+ *
+ * Return: 0 if Success
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
