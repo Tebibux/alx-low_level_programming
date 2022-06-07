e4d5584992ba8b0df48bd17bace7e5e974b1781c
tebibux <tebibusolomon79@gmail.com>
2022-06-07 14:30:47 -0700

    Task 7, last degit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last degit of the given intiger
+ *
+ * @n: is the value of an initger
+ *
+ * Return: the last digit of an intiger
+ */
+
+int print_last_digit(int)
+{
+	int n;
+	n = n%10;
+
+	return (n);
+}
