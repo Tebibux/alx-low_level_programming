c7ce78cb26354024f32c0169e3edd93889540f6f
tebibux <tebibusolomon79@gmail.com>
2022-06-09 14:57:42 -0700

    Task 3, Printing number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the number from 0-9
+ * @i: the nuber that printed
+ * Return: 0 if Success
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar("%d", i);
+	}
+	_putchar('\n');
+}
