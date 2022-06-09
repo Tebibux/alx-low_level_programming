b136abe0fa922ded9e47ce1f1cea44fdcb0c2ed8
tebibux <tebibusolomon79@gmail.com>
2022-06-09 14:46:32 -0700

    Task 2, returning the multiple of tow number

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - checks if parameter is an uppercase character.
+ * @a: input intiger.
+ * @b: input intiger.
+ * Return: return the multipilication of i*j.
+ */
+int mul(int a, int b)
+{
+	return (a*b);
+}
