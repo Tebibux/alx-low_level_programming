d3c54871190fed30b6aeb9c7c3bf2d601c47d5e5
tebibux <tebibusolomon79@gmail.com>
2022-06-13 13:18:13 -0700

    Task 1, swamping value

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - exchanges the value of one to the other
+ * @*a: the memory address of variable a
+ * @*b: the memory address of variable b
+ * Return: nothing 
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
