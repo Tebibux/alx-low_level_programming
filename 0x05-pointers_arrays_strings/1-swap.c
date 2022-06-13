b01b9a611bb4e87df37d4dc8883124077d23e40e
tebibux <tebibusolomon79@gmail.com>
2022-06-13 13:24:20 -0700

    Task 1, betty style

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,8 +4,7 @@
  * swap_int - exchanges the value of one to the other
  * @a: the memory address of variable a
  * @b: the memory address of variable b
- *
- * Return: nothing 
+ * Return: nothing
  */
 void swap_int(int *a, int *b)
 {
