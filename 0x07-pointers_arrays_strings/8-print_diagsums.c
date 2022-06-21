6b035326ab278cda928ca007b4f1d7e310f12aae
tebibux <tebibusolomon79@gmail.com>
2022-06-21 11:10:33 -0700

    Task 7, adding crisscross

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagsumes - print the diagonal sum with each side
+ * @i: iterator
+ * @sum1: the size + i
+ * @sum2: the size - i
+ * Return: null
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	while (i < (size * size))
+	{
+		if (i % (size + 1) == 0)
+		{
+			sum1 = sum1 + *(a + i);
+		}
+		if (i % (size - 1) == 0 && i != 0 && i < size *size -1)
+		{
+			sum2 = sum2 + *(a + 1);
+		}
+		i++;
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
