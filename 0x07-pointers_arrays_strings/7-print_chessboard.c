b3fde14291678b557b99b1fb894049d27ab3eedf
tebibux <tebibusolomon79@gmail.com>
2022-06-21 10:48:18 -0700

    Task 7, chessboard on the terminal

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -16,10 +16,10 @@ void print_chessboard(char (*a)[8])
 	{
 		if(i % 8 == 8 && i != 0)
 		{
-			m = i;
+			j = i;
 			_putchar('\n');
 		}
-		_putchar(a[i / 8][i - m]);
+		_putchar(a[i / 8][i - j]);
 		i++;
 	}
 	_putchar('\n');
