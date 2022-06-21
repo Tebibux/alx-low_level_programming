913798b252525c185bafebc5899a96b002ccaf41
tebibux <tebibusolomon79@gmail.com>
2022-06-21 10:46:03 -0700

    Task 7, chessboard on the terminal

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,24 +1,23 @@
 #include "main.h"
 
-
-
+/**
+ * print_chessboard - will print chessboard
+ * @i: row
+ * @j: colument
+ * Return:
+ */
 void print_chessboard(char (*a)[8])
 {
 	unsigned int i, j;
 	i = 0;
 	j = 0;
 
-	while (i < 9)
+	while (i < 64)
 	{
-		if(i%2 !=0)
-		{
-			_putchar('%d', m);
-		}
-		while (j < 9)
+		if(i % 8 == 8 && i != 0)
 		{
 			m = i;
 			_putchar('\n');
-			j++;
 		}
 		_putchar(a[i / 8][i - m]);
 		i++;
