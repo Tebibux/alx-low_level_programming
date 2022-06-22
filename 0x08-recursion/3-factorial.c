5cf68c04a4eb56dd1bc5410d97857f87b525d15a
tebibux <tebibusolomon79@gmail.com>
2022-06-22 13:56:16 -0700

    Task 3, fatorial of an initer

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - calculate the factorial of an intiger
+ * @n: intiger
+ * Return: return factorial 
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return -1;
+	}
+	else if (n)
+	{
+		return (n * factorial(n-1));
+	}
+	else
+	{
+		return 1;
+	}
+}
