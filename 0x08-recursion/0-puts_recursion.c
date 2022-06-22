1ed309755d3688e02a4d607680041f1bef826368
tebibux <tebibusolomon79@gmail.com>
2022-06-22 12:46:30 -0700

    Recursion and Task 0, printing string using pointer and resurion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - puts a sting in recurion
+ * @s: string that be in the sting 
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This part forucess on recursion
