5ffac080fc0d52b68b678a43cd418f4da4a723ac
tebibux <tebibusolomon79@gmail.com>
2022-06-22 13:07:12 -0700

    Task 1, revers string using recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints the revers of a string using recurion
+ * @s: pointer points the location of a character
+ * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\n')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar("%c", *s):
+	{
+}
