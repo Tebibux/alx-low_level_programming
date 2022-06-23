e28c9775d79708c541895fa08220e922b413ad37
tebibux <tebibusolomon79@gmail.com>
2022-06-23 11:25:13 -0700

    Task 5, square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - calculate the squar root of a number
+ * @n: an intiger to be calculated
+ * Return: the root of value of an intiger
+ */
+int _sqrt_recursion(int n)
+{
+	int x = 0.01;
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (i = x && i*i < n)
+	{
+		x++;
+		return _sqrt_recursion(n);
+	}
+	else
+	{
+		return (i);
+	}
+}
