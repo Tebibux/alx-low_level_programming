79f44262342135f463a5bade0be23f94ebc151a1
tebibux <tebibusolomon79@gmail.com>
2022-06-24 15:06:53 -0700

    Task 1, works, tested file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of argument 
+ * Return: 0 if seccess
+ */
+int main(int argc, char* argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
