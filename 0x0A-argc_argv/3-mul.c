1a2098ec99b4c2b2b1e9e4894ca2be9ef1054b22
tebibux <tebibusolomon79@gmail.com>
2022-06-24 15:38:00 -0700

    Task 3, tested works

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies the argument after the name
+ * @argc: intiger argument passed to the func
+ * @argv: the array of string
+ * @i: used to iterate 
+ * @mul: used to calculate the multips
+ * Return: 0 if success
+ */
+int main(int argc,char* argv[])
+{
+	int i;
+	int mul;
+	
+	mul = 1;
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	return (0);
+}
