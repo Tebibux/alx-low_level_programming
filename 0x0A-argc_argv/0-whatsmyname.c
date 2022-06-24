b4cee9dd85b4df0089b89d6b7108a156a0961850
tebibux <tebibusolomon79@gmail.com>
2022-06-24 14:47:57 -0700

    Task 0, second check

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,15 +2,11 @@
 
 /**
  * name - return the argument passed to pirnt the object
- * @i: the assigned intiger
  * Return: nothing
  */
 int name(int argc, char* argv[])
 {
-	int i;
-
-	i = 0;
-	if (i != '\0' && i == 0)
+	if (argc != '\0' && argc == 0)
 	{
 		_putchar("%s", argv[i]);
 	}
