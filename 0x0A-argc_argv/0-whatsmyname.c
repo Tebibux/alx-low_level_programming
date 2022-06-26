249bcd435a6e893ef5c4eb3801b86c4c5875a49b
tebibux <tebibusolomon79@gmail.com>
2022-06-26 12:25:11 -0700

    betty arregment

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,12 @@
 
 /**
  * main - return the argument passed to pirnt the object
+ * @argc: the  first argument 
+ * @argv: the seconde arguemnt 
  * Return: 0 if success
  */
 int main(int argc __attribute__((unused)), char* argv[])
 {
-	printf("%s\n", argv[0]);
+	printf(" %s\n", argv[0]);
 	return (0);
 }
