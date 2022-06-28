2450206f60c69e326ecc69c5ed10d12424c45e93
tebibux <tebibusolomon79@gmail.com>
2022-06-28 03:27:01 -0700

    malloc project

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates array with specific char 
+ * @size: size of array
+ * @c: specific char
+ * Return: return pointer if success
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *chr;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	chr = malloc(sizeof(c) * size);
+	
+	if (chr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		chr[i] = c
+	}
+	return (chr);
+
+}
