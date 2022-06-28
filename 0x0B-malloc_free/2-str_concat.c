f6278963d7bf9b0b9fca15164d240da32922863c
tebibux <tebibusolomon79@gmail.com>
2022-06-28 15:43:13 -0700

    Task 2, and task 1

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * str_concat - return pionter to newly allocated space
+ * @s1: the first string
+ * @s2: the second string
+ * Return: pointer is success
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char* ptr;
+	unsigned int i, j, k, limit;
+	
+	i = 0;
+	j = 0;
+	k = 0;
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		S2 = "";
+	}
+	while (s1[i] = '\0')
+	{
+		i++;
+	}
+	while (s2[j] = '\0')
+        {
+               	j++;
+        }
+	prt = malloc(sizeof(char) * (i + j + 1));
+	if (prt == NULL)
+	{
+		free (prt);
+		return (NULL);
+	}
+	while (k <= i)
+	{
+		prt[k] = s1 [k];
+	}
+	limit = j;
+	while (j <= limit)
+	{
+		prt[k] = s2[j];
+	}
+	return (prt);
+}
