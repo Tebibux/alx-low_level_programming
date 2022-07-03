9030a5b12c7c72f80ba39c8df097a25db0f8afaf
tebibux <tebibusolomon79@gmail.com>
2022-07-03 13:48:42 -0700

    Task 4, works

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,30 +1,41 @@
 #include <stdio.h>
+#include "main.h"
+#include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main: adds positive nubmers
+ * main: add number that passed after the number is include
  * @argc: nuber of command
  * @argv: array of that command
  * Return: 0 - success
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	int i;
-	int j;
-	int sum = 0;
+	int i, j, length, sum;
+	char *ptr;
 
-	for (i = 1; i <= argc; i++)
+	if (argc < 2)
+	printf("0\n");
+	else
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		sum = 0;
+		for (i = 1; i < argc; i++)
 		{
-			if (!isdigit(argv[i][j]))
+			ptr = argv[i];
+			length = strlen(ptr);
+			for (j = 0; j < length; j++)
 			{
-				printf("Error\n");
-				return (1);
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-		sum += atoi(argv[i]);
+
+			sum += atoi(argv[i]);
 		}
+		printf("%d\n", sum);
 	}
-	printf("%d\n", sum);
 	return (0);
 }
