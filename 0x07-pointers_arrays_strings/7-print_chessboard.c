58de4774da1d8d8c51f0307f50ff750f01895c8d
tebibux <tebibusolomon79@gmail.com>
2022-07-10 08:43:05 -0700

    Task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,25 +2,23 @@
 
 /**
  * print_chessboard - will print chessboard
- * @i: row
- * @j: colument
+ * @a: the chessboard
  * Return:
  */
 void print_chessboard(char (*a)[8])
 {
-	unsigned int i, j;
-	i = 0;
-	j = 0;
+	int i, j;
 
-	while (i < 64)
+	i = 0;
+	while (a[i][7])
 	{
-		if(i % 8 == 8 && i != 0)
+		j = 0;
+		while (j < 8)
 		{
-			j = i;
-			_putchar('\n');
+			_putchar(a[i][j]);
+			j++;
 		}
-		_putchar(a[i / 8][i - j]);
+		_putchar('\n');
 		i++;
 	}
-	_putchar('\n');
 }
